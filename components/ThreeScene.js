6653949d5f05b06b56ebd471f9e2351c1d0f274f
Kasi Reeves <Kasirocswell@Rocketmail.com>
2023-07-22 07:36:24 -0700

    Fixed Model Paths

--- components/ThreeScene.js
+++ components/ThreeScene.js
@@ -3,10 +3,10 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const modelPaths = [
-  "/static/juice02.glb",
-  "/static/juice03.glb",
-  "/static/juice04.glb",
-  "/static/juice05.glb",
+  "/juice02.glb",
+  "/juice03.glb",
+  "/juice04.glb",
+  "/juice05.glb",
 ];
 
 const ThreeScene = () => {
